5943d951b724b9091b1706b1ae1a9c01baec3e9e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-18 01:03:04 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
-    console.log(e.currentTarget);
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
   });
 });
